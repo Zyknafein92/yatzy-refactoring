7e39461155e82a6a1600c4c63352d1636367fda5
Zyknafein <deneux.j@gmail.com>
2022-04-19 18:55:50 +0200

    Delete useless languages folder, Refactor Yatzy methods, and constructor Refactor Yatzy test

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,240 +1,176 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    protected int[] dices; //array with the roll of all dices.
+    protected int betNumber; // number who represent the bet of Player for Ones,Twos,Threes,Fours,Fives,Sixes
+
+    public Yatzy() {
+        this.dices = new int[5];
     }
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
+    public Yatzy(int[] dices, int betNumber) {
+        this.dices = new int[5];
+        this.betNumber = betNumber;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+    /**
+     * This method do the sum of the five dices in they array.
+     * @param dices array with the roll of all dices
+     * @return int of the sum of array dices
+     */
+    public static int chance(int[] dices) {
+        return Arrays.stream(dices).sum();
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+    /**
+     * This method check with a HashSet collection if all values are unique.
+     * If they are, it will return 50, if not 0
+     * @param dices array with the roll of all dices
+     * @return int with value 50 if values are unique, or 0 if not.
+     */
+    public static int yatzy(int[] dices) {
+        HashSet<Integer> diceResult = new HashSet<>();
+        Arrays.stream(dices).forEach(diceResult::add);
+        return diceResult.size() == 1 ? 50 : 0;
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+    /**
+     * This method replace Ones,Twos,Threes,Fours,Fives,Sixes
+     * Player bet on a number, and the method will use a Map to get all numbers
+     * and how many times they are present.
+     * If the map contains the key, the method will return map.value * betNumber,
+     * If not, it return 0.
+     * @param dices array with the roll of all dices
+     * @param betNumber int number who represent the bet of Player for Ones,Twos,Threes,Fours,Fives,Sixes
+     * @return int with sum of betNumber * map.value or 0
+     */
+    public static int betOnNumber(int[] dices, int betNumber) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        return diceMap.containsKey(betNumber) ? diceMap.get(betNumber) * betNumber : 0;
     }
 
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
-    }
-
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
+    /**
+     * This method will check if the array of dices contains a pair and will take the highest
+     * if they are multiples by using a map for count the number of dice with the same value.
+     * After the map, we use a filter to get all values superior at 2 for get all pairs and
+     * we use Collections.max to get the highest pair value.
+     * Finally, we multiply the value of the key by two to get the score.
+     * @param dices array with the roll of all dices
+     * @return int value of the hightest pair number multiply by 2 or 0.
+     */
+    public static int onePair(int[] dices) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        List<Integer> filterMap = diceMap.keySet().stream().filter((v) -> diceMap.get(v) >= 2).collect(Collectors.toList());
+        if (filterMap.isEmpty()) {
+            return 0;
+        } else {
+            Integer key = Collections.max(filterMap);
+            return key * 2;
         }
-        return sum;
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
-    }
-
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
-    }
-
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
-    }
-
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
+    /**
+     * This method will check if the array of dices contains two pairs.
+     * We use a map for count the number of dice with the same value.
+     * After the map, we use a filter to get all pairs and get a list of the pairs.
+     * If there is two pairs, we will use the list obtain by the filter and reduce it
+     * to the sum of the key values multiply by 2.
+     * If not, we return 0.
+     * @param dices array with the roll of all dices
+     * @return int value of all pairs multiply by 2 or 0.
+     */
+    public static int twoPair(int[] dices) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        List<Integer> filterMap = diceMap.keySet().stream().filter((v) -> diceMap.get(v) >= 2).collect(Collectors.toList());
+        if(filterMap.size() == 2) {
+            return filterMap.stream().reduce(0, Integer::sum) * 2;
+        } else {
             return 0;
+        }
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    /**
+     * This method will check if there is three dices with the same value in the array.
+     * We use a map for count the number of dice with the same value.
+     * After we filter the map to get the key who his present 3 times or if not, we return 0.
+     * We return the value of the filter multiply by 3.
+     * @param dices array with the roll of all dices.
+     * @return int value of the key multiply by 3.
+     */
+    public static int threeOfAKind(int[] dices) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        Integer key = diceMap.keySet().stream().filter(k -> diceMap.get(k) >= 3).findFirst().orElse(0);
+        return key * 3;
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+    /**
+     * This method will check if there is three dices with the same value in the array.
+     * We use a map for count the number of dice with the same value.
+     * After we filter the map to get the key who his present 4 times or if not, we return 0.
+     * We return the value of the filter multiply by 4.
+     * @param dices array with the roll of all dices.
+     * @return int value of the key multiply by 4.
+     */
+    public static int fourOfAKind(int[] dices) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        Integer key = diceMap.keySet().stream().filter(k -> diceMap.get(k) >= 4).findFirst().orElse(0);
+        return key * 4;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
-    }
-
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
-    }
-
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
+    /**
+     * This method will check if there is a small straight or full straight and return the score value.
+     * We use a Set for check if all dices values are uniques.
+     * If they are, we check if 1 or 6 is present, if 1 it's a small straight, if 6 it's a large straight
+     * and we return the score for the straight value.
+     * If all values are not unique, we return 0
+     * @param dices array with the roll of all dices.
+     * @return int value of the straight score
+     */
+    public static int straight(int[] dices) {
+        Set<Integer> diceSet = new HashSet<>();
+        Arrays.stream(dices).forEach(diceSet::add);
+        if(diceSet.size() != 5) {
             return 0;
+        }
+        return diceSet.contains(1) ? 15 : 20;
     }
+
+    /**
+     * This method will check if dices number are three of a kind and two of a king.
+     * We use a map for count the number of dice with the same value.
+     * We check is the map size is 2 and if the numbers present in the map are superior to 1
+     * If they are we return the sum of the values
+     * If not, we return 0
+     * @param dices array with the roll of all dices.
+     * @return int value of array dices sum
+     */
+    public static int fullHouse(int[] dices) {
+        HashMap<Integer, Integer> diceMap = convertArrayToMap(dices);
+        if(diceMap.size() != 2 || diceMap.containsValue(1)) {
+            return 0;
+        } else {
+            return Arrays.stream(dices).sum();
+        }
+    }
+
+    /**
+     * This method will convert the array dices to a map.
+     * @param dices array with the roll of all dices.
+     * @return map with for key the number, and for the value of times it is present
+     */
+    private static HashMap<Integer, Integer> convertArrayToMap(int[] dices) {
+        HashMap<Integer, Integer> diceMap = new HashMap<>();
+        Arrays.stream(dices).forEach(d -> {
+            if (diceMap.containsKey(d)) {
+                diceMap.put(d, diceMap.get(d) + 1);
+            }
+            diceMap.putIfAbsent(d, 1);
+        });
+        return diceMap;
+    }
+
 }
 
 
--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -4,107 +4,125 @@ import static org.junit.Assert.*;
 public class YatzyTest {
 
     @Test
-    public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
-    }
-
-    @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
-    }
-
-    @Test public void test_1s() {
-        assertTrue(Yatzy.ones(1,2,3,4,5) == 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+    public void testChanceGivenValueReturnSum() {
+        assertEquals(15, Yatzy.chance(new int[]{2, 3, 4, 5, 1}));
+        assertEquals(16, Yatzy.chance(new int[]{3,3,4,5,1}));
     }
 
     @Test
-    public void test_2s() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
+    public void testYatzyGivenInvalidValueReturnZero() {
+        assertEquals(0, Yatzy.yatzy(new int[]{2, 3, 4, 5, 1}));
+        assertEquals(0, Yatzy.yatzy(new int[]{1, 1, 4, 5, 1}));
     }
 
     @Test
-    public void test_threes() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
+    public void testYatzyGivenValueReturn50() {
+        assertEquals(50, Yatzy.yatzy(new int[]{2, 2, 2, 2, 2}));
+        assertEquals(50, Yatzy.yatzy(new int[]{1, 1, 1, 1, 1}));
     }
 
     @Test
-    public void fours_test() 
-    {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+    public void testBetOnNumberGivenInvalidValueReturnZero () {
+        assertEquals(0, Yatzy.betOnNumber(new int[]{2, 2, 2, 2, 2},3));
+        assertEquals(0, Yatzy.betOnNumber(new int[]{1, 3, 2, 1, 5},6));
     }
 
     @Test
-    public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+    public void testBetOnNumberGivenValueReturnSumBetNumberXDiceNumbersWithBetValue () {
+        assertEquals(10, Yatzy.betOnNumber(new int[]{2, 2, 2, 2, 2},2));
+        assertEquals(2, Yatzy.betOnNumber(new int[]{1, 3, 2, 1, 5},1));
     }
 
     @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+    public void testOnePaireGivenInvalidValueReturnZero () {
+        assertEquals(0, Yatzy.onePair(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(0, Yatzy.onePair(new int[]{3, 2, 4, 5, 6}));
     }
 
     @Test
-    public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+    public void testOnePaireGivenValueReturnSumOfPairValue () {
+        assertEquals(8, Yatzy.onePair(new int[]{1, 2, 6, 4, 4}));
+        assertEquals(4, Yatzy.onePair(new int[]{3, 2, 2, 5, 6}));
     }
 
     @Test
-    public void two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
+    public void testOnePaireGivenValueWithTwoPairReturnHighestSumOfPairValue () {
+        assertEquals(8, Yatzy.onePair(new int[]{1, 2, 2, 4, 4}));
+        assertEquals(10, Yatzy.onePair(new int[]{3, 2, 2, 5, 5}));
     }
 
     @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+    public void testTwoPaireGivenInvalidValueReturnZero () {
+        assertEquals(0, Yatzy.twoPair(new int[]{1, 2, 1, 5, 4}));
+        assertEquals(0, Yatzy.twoPair(new int[]{3, 2, 1, 5, 5}));
     }
 
     @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+    public void testTwoPaireGivenValueReturnSumOfTwoPairValues () {
+        assertEquals(6, Yatzy.twoPair(new int[]{1, 2, 2, 5, 1}));
+        assertEquals(16, Yatzy.twoPair(new int[]{3, 3, 1, 5, 5}));
     }
 
     @Test
-    public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
+    public void testThreeOfAKingGivenInvalidValueReturnZero () {
+        assertEquals(0, Yatzy.threeOfAKind(new int[]{1, 2, 2, 5, 1}));
+        assertEquals(0, Yatzy.threeOfAKind(new int[]{3, 3, 1, 5, 5}));
+        assertEquals(0, Yatzy.threeOfAKind(new int[]{1, 2, 3, 5, 6}));
     }
 
     @Test
-    public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
+    public void testThreeOfAKingGivenInvalidValueReturnSumOfValues () {
+        assertEquals(3, Yatzy.threeOfAKind(new int[]{1, 1, 2, 5, 1}));
+        assertEquals(15, Yatzy.threeOfAKind(new int[]{3, 5, 1, 5, 5}));
     }
 
     @Test
-    public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+    public void testFourOfAKingGivenInvalidValueReturnZero () {
+        assertEquals(0, Yatzy.fourOfAKind(new int[]{1, 2, 2, 5, 1}));
+        assertEquals(0, Yatzy.fourOfAKind(new int[]{3, 3, 1, 5, 5}));
+        assertEquals(0, Yatzy.fourOfAKind(new int[]{1, 2, 3, 5, 6}));
+    }
+
+    @Test
+    public void testFourOfAKingGivenInvalidValueReturnSumOfValues () {
+        assertEquals(4, Yatzy.fourOfAKind(new int[]{1, 1, 1, 5, 1}));
+        assertEquals(20, Yatzy.fourOfAKind(new int[]{5, 5, 1, 5, 5}));
+    }
+
+    @Test
+    public void testStraightGivenInvalidValuesReturnZero() {
+        assertEquals(0, Yatzy.straight(new int[]{1,2,2,5,5}));
+        assertEquals(0, Yatzy.straight(new int[]{1,2,1,4,5}));
+    }
+
+    @Test
+    public void testStraightGivenSmallStraightReturn15() {
+        assertEquals(15, Yatzy.straight(new int[]{1,2,3,4,5}));
+        assertEquals(15, Yatzy.straight(new int[]{2,3,4,5,1}));
+    }
+
+    @Test
+    public void testStraightGivenLargeStraightReturn20() {
+        assertEquals(20, Yatzy.straight(new int[]{6,2,3,4,5}));
+        assertEquals(20, Yatzy.straight(new int[]{2,3,4,5,6}));
+    }
+
+    @Test
+    public void testFullHouseGivenInvalidValueReturnSumOfValues() {
+        assertEquals(0, Yatzy.fullHouse(new int[]{2,3,4,5,6}));
+        assertEquals(0, Yatzy.fullHouse(new int[]{3,3,4,5,6}));
+        assertEquals(0, Yatzy.fullHouse(new int[]{3,3,5,5,6}));
+    }
+
+    @Test
+    public void testFullHouseGivenTwoDifferentDicesValueWithOneUniqueNumberReturnZero() {
+        assertEquals(0, Yatzy.fullHouse(new int[]{1,1,1,1,5}));
+    }
+
+    @Test
+    public void testFullHouseGivenValidValueReturnSumOfValues() {
+        assertEquals(18, Yatzy.fullHouse(new int[]{6,2,2,2,6}));
+        assertEquals(14, Yatzy.fullHouse(new int[]{2,2,2,4,4}));
+        assertEquals(14, Yatzy.fullHouse(new int[]{2,4,2,2,4}));
     }
 }
